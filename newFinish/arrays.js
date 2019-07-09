b37aefdd48ccda3aca9ccefffb2dfb01fe3fd856
yoda14 <choobthenoob@hotmail.com>
2019-07-09 14:15:08 +0800

    Levels to infinity!!

--- newFinish/arrays.js
+++ newFinish/arrays.js
@@ -7,33 +7,6 @@ function getRandomColor() {
   return color;
 }
 
-document.getElementById("gallery").addEventListener("click", firstGame);
-
-function firstGame(){
-    document.getElementById("story").innerHTML = "You get six tries to find me. Click one of the colorful boxes to find me.";
-    var square = document.getElementById("canvas");
-    var degree = 20;
-    for (i=1; i<19; i++){
-        var makeDiv = document.createElement("div");
-        makeDiv.setAttribute("class","firstGameBox");
-        makeDiv.style.backgroundColor = getRandomColor();
-        makeDiv.setAttribute("id","firstGameBox-"+i);
-        makeDiv.style.transform = "rotate("+degree+"deg)";
-        square.appendChild(makeDiv);
-        degree = degree + 20;
-        document.getElementById("firstGameBox-"+i).addEventListener("mouseover", function(){
-                this.style.backgroundColor = getRandomColor();
-            });
-        document.getElementById("firstGameBox-"+i).addEventListener("click", firstGameCheck);
-    }
-    this.remove();
-}
-
-var firstGameCounter = 6;
-
-var firstGameRandomBox = "firstGameBox-" + Math.ceil(Math.random()*18);
-console.log(firstGameRandomBox);
-
 function pageReload(){
     location.reload();
 }
@@ -54,29 +27,64 @@ document.getElementById("reloadDiv").addEventListener("click",
     pageReload);
 };
 
-function firstGameCheck(event){
-        if (event.target.id === firstGameRandomBox){
-            document.getElementById("output").innerHTML = "You found me!";
-            clearContent('firstGameBox');
-            document.getElementById("story").innerHTML = "Click me to enter the next level.";
-            document.getElementById("story").addEventListener("click", secondGame);
+document.getElementById("gallery").addEventListener("click", firstGame);
+
+var firstGameCounter;
+var firstGameRandomBox;
+var levelCounter = 0;
+
+function firstGame(){
+    firstGameCounter = 6;
+    firstGameRandomBox = "firstGameBox-" + Math.ceil(Math.random()*18);
+    console.log(firstGameRandomBox);
+    document.getElementById("story").innerHTML = "You get six tries to find me. Click one of the colorful boxes to find me.";
+    levelCounter++;
+    document.getElementById("output").innerHTML="Level "+levelCounter;
+    var square = document.getElementById("canvas");
+    var degree = 20;
+    for (i=1; i<19; i++){
+        var makeDiv = document.createElement("div");
+        makeDiv.setAttribute("class","firstGameBox");
+        makeDiv.style.backgroundColor = getRandomColor();
+        makeDiv.setAttribute("id","firstGameBox-"+i);
+        makeDiv.style.transform = "rotate("+degree+"deg)";
+        square.appendChild(makeDiv);
+        degree = degree + 20;
+        document.getElementById("firstGameBox-"+i).addEventListener("mouseover", function(){
+                this.style.backgroundColor = getRandomColor();
+            });
+        document.getElementById("firstGameBox-"+i).addEventListener("click", firstGameCheck);
+    }
+    this.remove();
+}
+
+function firstGameCheck(){
+    if (event.target.id === firstGameRandomBox){
+        document.getElementById("output").innerHTML = "You found me!";
+        clearContent('firstGameBox');
+        document.getElementById("story").innerHTML = "Click me to enter the next level.";
+        document.getElementById("story").addEventListener("click", secondGame);
         }else{
             event.target.remove();
             firstGameCounter--;
             document.getElementById("output").innerHTML = "You have "+firstGameCounter+" tries left!";
             if(firstGameCounter === 0){
-        document.getElementById("output").innerHTML = "GAME OVER!";
-        createReload();
-    clearContent('firstGameBox');
-    }
+                document.getElementById("output").innerHTML = "GAME OVER!";
+                createReload();
+                clearContent('firstGameBox');
+            }
         }
     };
 
-var secondGameCounter = 6;
-var secondGameRandomBox = "secondGameBox-" + Math.ceil(Math.random()*12);
-console.log(secondGameRandomBox);
+var secondGameCounter;
+var secondGameRandomBox;
 
 function secondGame(){
+    secondGameCounter = 6;
+    secondGameRandomBox = "secondGameBox-" + Math.ceil(Math.random()*12);
+    console.log(secondGameRandomBox);
+    levelCounter++;
+    document.getElementById("output").innerHTML="Level "+levelCounter;
     var square = document.getElementById("canvas");
     square.setAttribute("class", "stage");
     console.log("Second game is running now!");
@@ -97,22 +105,24 @@ function secondGame(){
     var topRightDiv = document.createElement("div");
     topRightDiv.setAttribute("class", "topRight");
     topRightDiv.setAttribute("id", "story");
-    topRightDiv.innerHTML = "You're in level two! You have six tries to find me again!";
+    topRightDiv.innerHTML = "You're in level "+levelCounter+"! You have six tries to find me again!";
     insertPlace.appendChild(topRightDiv);
     var bottomRightDiv = document.createElement("div");
     bottomRightDiv.setAttribute("class", "bottomRight");
     bottomRightDiv.setAttribute("id", "gallery");
     bottomRightDiv.innerHTML = "Click boxes!";
     insertPlace.appendChild(bottomRightDiv);
-    document.getElementById("output").innerHTML = "Level 2";
 }
 
-function secondGameCheck(event){
+function secondGameCheck(){
         if (event.target.id === secondGameRandomBox){
             document.getElementById("output").innerHTML = "You found me!";
             clearContent('secondGameBox');
-            document.getElementById("story").innerHTML = "Click me to enter the next level.";
-            // document.getElementById("smallControl").addEventListener("click", secondGame);
+            document.getElementById("gallery").innerHTML = "Next level!";
+            var square = document.getElementById("canvas");
+            square.setAttribute("class", "topLeft");
+            document.getElementById("gallery").addEventListener
+            ("click", firstGame);
         }else{
             event.target.remove();
             secondGameCounter--;
